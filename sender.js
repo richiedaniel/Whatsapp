b00836849451d6ded55abeda20e266e5021ceccc
Richie Daniel <44722445+richiedaniel@users.noreply.github.com>
2023-04-13 17:15:36 +0800

    Update sender.js
    2023

--- sender.js
+++ sender.js
@@ -2,21 +2,20 @@ function kirimpesan(nomor,pesan) {
 var tempatnomorbaru=document.getElementById("nomorwa");
 if(tempatnomorbaru==null){
   var href = document.createElement("a");
-  href.href = "http://wa.me/"+nomor;
+  href.href = "http://wa.me/"+nomor+"?text="+encodeURIComponent(pesan);
   href.innerText= "00000";
   href.setAttribute("id", "nomorwa");
   document.body.appendChild(href);
 }else{
-  tempatnomorbaru.href= "http://wa.me/"+nomor;
+  tempatnomorbaru.href= "http://wa.me/"+nomor+"?text="+encodeURIComponent(pesan);
 }
 document.querySelector("#nomorwa").click()
 setTimeout(() => {
-messageBox = document.querySelectorAll("[contenteditable='true']")[1]; 
 event = document.createEvent("UIEvents"); 
-messageBox.innerHTML = '<p class="selectable-text copyable-text iq0m558w" dir="ltr"><span class="selectable-text copyable-text" data-lexical-text="true">${pesan}</span></p>';
 event.initUIEvent("input", true, true, window, 1); 
-messageBox.dispatchEvent(event); 
 document.querySelector('span[data-icon="send"]').click();
 console.log(nomor+" Berhasil");
-}, 600);
+}, 3000);
 }
+//Eksekusi    
+kirimpesan("628111111","OTP KAMU ADALAH 63733");
